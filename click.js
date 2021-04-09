e75099edead5c90dff8f0bf0ab7592f6c5f851bd
xxDEDExx <75202314+xxDEDExx@users.noreply.github.com>
2021-04-09 14:03:21 +0200

    Add files via upload

---
+++ click.js
@@ -0,0 +1,5 @@
+$(document).ready(function(){
+    $('.fa-cart-plus').click(function(){
+        $('#cart-items').toggle();
+    });
+});
---
+++ products.js
@@ -0,0 +1,63 @@
+// DUMMY PRODUCTS (PRODUCT ID : DATA)
+var products = {
+  123: {
+    name : "Enterpreneur Bed",
+    img : "product-2.jpeg",
+    price : 2034
+  },
+  124: {
+    name : "Queen's Bed",
+    img : "product-3.jpeg",
+    price : 747
+  },
+  125: {
+    name : "Single Bed",
+    img : "product-4.jpeg",
+    price : 175
+  },
+  126: {
+    name : "Kitchen Stools",
+    img : "product-5.jpeg",
+    price : 42
+  },
+  127: {
+    name : "Headrest Sofa",
+    img : "product-6.jpeg",
+    price : 120
+  },
+  128: {
+    name : "Orion's Sofa",
+    img : "product-7.jpeg",
+    price : 375
+  },
+  129: {
+    name : "Lix Chair",
+    img : "product-8.jpeg",
+    price : 142
+  },
+  130: {
+    name : "Corner Desk",
+    img : "10.jpg",
+    price : 90
+  },
+  131: {
+    name : "Square Shelves",
+    img : "11.jpg",
+    price : 85
+  },
+  132: {
+    name : "Cordless Clean",
+    img : "13.jpg",
+    price : 842
+  },
+  133: {
+    name : "Mono Kids",
+    img : "12.jpg",
+    price : 247
+  },
+  134: {
+    name : "Smart Light",
+    img : "light.jpg",
+    price : 75
+  },
+};
